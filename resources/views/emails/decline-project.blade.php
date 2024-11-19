ac69c5c139e6b538c8aca1e9f3ef8a2b80a6bda7
Artsandy Abing <artsandyabing@gmail.com>
2024-11-19 15:29:51 +0800

    updated

--- resources/views/emails/decline-project.blade.php
+++ resources/views/emails/decline-project.blade.php
@@ -9,7 +9,7 @@
     <div style="max-width: 600px; margin: auto; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 20px;">
         <h1 style="color: #568d2e; text-align: center;">Client Project</h1>
         <p style="font-size: 16px;">Hello <strong>{{ $user->name }}</strong>,</p>
-        <p style="font-size: 16px;">Unfortunately, your project request has been approved due to some reason.</p>
+        <p style="font-size: 16px;">Unfortunately, your project request has been declined due to some reason.</p>
         <p style="font-size: 16px;">Kindly contact us via Smart +639217930608, Sun +639335904190.</p>
         <p style="font-size: 16px;">Or you may email us at jigger_132005@yahoo.com.</p>
       </div>
