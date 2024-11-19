56697afec72e6d3ac517b45a4277b2159566d9ad
Artsandy Abing <artsandyabing@gmail.com>
2024-11-19 16:56:45 +0800

    updated

--- resources/views/emails/decline-payment.blade.php
+++ resources/views/emails/decline-payment.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Client Project</title>
+    <title>Client Payment</title>
 </head>
 <body style="font-family: Verdana, Geneva, sans-serif; background-color: #f2f2f2; color: #333; margin: 0; padding: 20px;">
     <div style="max-width: 600px; margin: auto; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 20px;">
-        <h1 style="color: #568d2e; text-align: center;">Client Project</h1>
+        <h1 style="color: #568d2e; text-align: center;">Client Payment</h1>
         <p style="font-size: 16px;">Hello <strong>{{ $user->name }}</strong>,</p>
         <p style="font-size: 16px;">Unfortunately, your payment request has been delined due to some reason.</p>
         <p style="font-size: 16px;">Kindly contact us via Smart +639217930608, Sun +639335904190.</p>
